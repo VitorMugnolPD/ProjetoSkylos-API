07e188ae5a14990d7fc481534af0917725f3f861
Bruno <bruno.franchi@live.com>
2020-05-25 14:23:41 -0300

    adicao do email para os cuidadores e clientes

--- ProjetoSkylos_API/Models/Cliente.cs
+++ ProjetoSkylos_API/Models/Cliente.cs
@@ -5,8 +5,8 @@ namespace ProjetoSkylos_API.Models
         public int Id {get; set;}
         public string Nome {get; set;}
         public string Senha {get; set;}
+        public string Email {get; set;}
         public double Latitude{get; set;}
-
         public double Longitude {get; set;} 
     }
 }
--- ProjetoSkylos_API/Models/Cuidador.cs
+++ ProjetoSkylos_API/Models/Cuidador.cs
@@ -5,10 +5,9 @@ namespace ProjetoSkylos_API.Models
         public int Id {get; set;}
         public string Nome {get; set;}
         public string Senha {get; set;}
+        public string Email {get; set;}
         public double Latitude{get; set;}
-
         public double Longitude {get; set;} 
-
         public string AfinidadeComBichos{get;set;}
     }
 }
